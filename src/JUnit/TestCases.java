a249fcf2bd6486b023e5f3c7e7d4ec70461ce3ae
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-04-12 18:44:30 -0500

    Added JUnit

---
+++ src/JUnit/TestCases.java
@@ -0,0 +1,14 @@
+package JUnit;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCases {
+
+	@Test
+	public void t1 () {
+		assertEquals("Here is the test for multiplication", 200, 200);
+	}
+	
+}
