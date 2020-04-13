faed092f7ca62067c870efa375eb22d8cfd382f0
Yousuf Jazzar <43428748+YouJazz@users.noreply.github.com>
2020-04-12 20:18:21 -0500

    Added custom exception

--- src/JUnit/TestCases.java
+++ src/JUnit/TestCases.java
@@ -9,41 +9,61 @@ public class TestCases {
 	@Test
 	public void case1 () {
 		Events events = new Events();
-		events.addEvent(new Event("Event 1", 10)));
+		events.addEvent(new Event("Event 1", 10));
 		
 		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 		
-	@Test
+	@Test (expected = ClassExceptionError.class)
 	public void case2 () {
-		assertEquals("Here is the test for multiplication", 200, 200);
+		Events events = new Events();
+		events.addEvent(new Event("Event 1", 10));
+		
+		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 	
 	@Test
 	public void case3 () {
-		assertEquals("Here is the test for multiplication", 200, 200);
+		Events events = new Events();
+		events.addEvent(new Event("Event 1", 10));
+		
+		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 	
 	@Test
 	public void case4 () {
-		assertEquals("Here is the test for multiplication", 200, 200);
+		Events events = new Events();
+		events.addEvent(new Event("Event 1", 10));
+		
+		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 	
 	@Test
 	public void case5 () {
-		assertEquals("Here is the test for multiplication", 200, 200);
+		Events events = new Events();
+		events.addEvent(new Event("Event 1", 10));
+		
+		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 	
 	@Test
 	public void case6 () {
-		assertEquals("Here is the test for multiplication", 200, 200);
+		Events events = new Events();
+		events.addEvent(new Event("Event 1", 10));
+		
+		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 	
 	@Test
 	public void case7 () {
-		assertEquals("Here is the test for multiplication", 200, 200);
+		Events events = new Events();
+		events.addEvent(new Event("Event 1", 10));
+		
+		assertEquals("Checking if event added successfully", 1, events.getSize());
 	}
 	
 	
 	
 }
+
+class ClassException extends Exception { }
