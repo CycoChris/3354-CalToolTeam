d690af7468d0200e39ad5e14f9f3299674097891
Albert Jean <albert.jean.mc@gmail.com>
2020-04-16 11:30:31 -0500

    Added implementation and test cases to Deliverable 2.

---
+++ CS3354-ProjectDel2-CalToolTeam/Implementation Code/com/CalTool/Main.java
@@ -0,0 +1,14 @@
+package com.CalTool;
+
+//import com.CalTool.GUI.CalendarView;
+
+public class Main {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("Works!");
+//		
+//		new CalendarView();
+//	}
+
+}
---
+++ CS3354-ProjectDel2-CalToolTeam/Test Code/JUnit/JUnitTester.java
@@ -0,0 +1,17 @@
+package JUnit;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class JUnitTester {
+
+	public static void main(String[] args) {
+	      Result result = JUnitCore.runClasses(TestCases.class);
+	      for (Failure failure : result.getFailures()) {
+	         System.out.println(failure.toString());
+	      }
+	      System.out.println("Result = " + result.wasSuccessful());
+	}
+	
+}
