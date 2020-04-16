2d69eaab173ca56aaf7ed69a34d41a1f82b6d9a2
Albert Jean <albert.jean.mc@gmail.com>
2020-04-16 11:34:54 -0500

    Updated main class.

--- src/com/CalTool/Main.java
+++ src/com/CalTool/Main.java
@@ -5,9 +5,6 @@ package com.CalTool;
 public class Main {
 
 //	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//		System.out.println("Works!");
-//		
 //		new CalendarView();
 //	}
 
